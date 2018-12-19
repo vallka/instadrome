30ef1bee8d9a8cfebff8f350888bc66fa4c727c8
Valentine Kulikov <vallka@vallka.com>
2018-12-19 01:07:19 +0000

    Revert "Revert "dirs as hash""
    This reverts commit 60d68245df4ba034ad89753b49eaca1591199503.

---
+++ gellifique/config/parameters.php
@@ -0,0 +1,28 @@
+<?php return array (
+  'parameters' => 
+  array (
+    //'database_host' => 'db620172445.db.1and1.com',
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'photo',
+    'database_user' => 'dbo620172445',
+    'database_password' => 'dobroskokina2',
+    'database_prefix' => 'gel_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+
+    'database_suffix' => '',
+    'instagram_user' => 'gellifiquegel',
+    'instagram_password' => 'Marusya1',
+
+    'instagram_tags' => ['gelbottle','thegelbottle','biab', 'premiergel','thegelbottleinc'],
+    'instagram_ignore' => ['the_gelbottle_inc','thegelbottlenz','thegelbottleireland',
+                            'thegelbottlegreece','thegelbottlebenelux','thegelbottleaus'
+                            ,'thegelbottlesweden','thegelbottlefrance','thegelbottlemalta','premier.gel'],
+
+
+  ),
+);
